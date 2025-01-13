b7bd04d81463cc211d87c2b550fb449ab89865f7
aatieh_42 <amjdsalh@gmail.com>
2025-01-13 03:16:20 +0300

    tokanizer start

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aatieh <aatieh@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 00:29:45 by yhamdan           #+#    #+#             */
-/*   Updated: 2025/01/10 20:25:03 by aatieh           ###   ########.fr       */
+/*   Updated: 2025/01/13 03:12:18 by aatieh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,43 +65,118 @@ int	words_count_sh(char *line)
 			word++;
 		i += j;
 	}
-	printf("wod count %d\n", word);
 	return (word);
 }
 
-int	word_len_sh(char *line, int *j)
+// void	get_variable_len(char **env, char *line, int *j, int *i)
+// {
+// 	int		n;
+// 	int		m;
+// 	char	*var_value;
+
+// 	n = *j;
+// 	while (line[*j] != ' ' && line[*j] != '\'' && line[*j] != '"')
+// 		*j++;
+// 	n -= *j;
+// 	m = 0;
+// 	while (env[m] && ft_strncmp(env[m], line[*j], n))
+// 		m++;
+// 	*i += ft_strlen(env[m]) - n;
+// }
+
+char	*get_variable(char **env, char *line, int *j, int *i)
+{
+	int		n;
+	int		m;
+	char	*var_value;
+
+	n = *j;
+	while (line[*j] != ' ' && line[*j] != '\'' && line[*j] != '"')
+		*j++;
+	n -= *j;
+	m = 0;
+	while (env[m] && ft_strncmp(env[m], line[*j], n) && env[m][n + 1] != '=')
+		m++;
+	if (!env[m])
+		return (NULL);
+	var_value = ft_strdup(env[m] + n + 1);
+	*i += n;
+	return (var_value);
+}
+
+int	word_len_sh(char *line, int *j, char **env)
 {
 	int	flag_q;
 	int	i;
 
-
 	if (!line)
 		return (NULL);
 	flag_q = 0;
-	i = j;
 	while (line[*j] && (line[*j] != ' ' || flag_q))
 	{
-		while ()
+		if (line[*j] == '\'' && !flag_q)
+			flag_q = 1;
+		else if (line[*j] == '"' && !flag_q)
+			flag_q = 2;
+		else if ((line[*j] == '\'' && flag_q == 1) || (line[*j] == '"' && flag_q == 2))
+			flag_q = 0;
+		if (line[*j] == '$' && flag_q != 1)
+			get_variable();
+		else
+		{
+			i++;
+			*j++;
+		}
 	}
 }
 
-char	*get_word_sh(char *line, int *j)
+char	*sh_strdup(const char *s, int j, int i)
+{
+	int	i;
+	char *res;
+	
+	res = (char *)malloc(sizeof(char) * (j + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < j)
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*get_token_sh(char *line, int *j, char **env)
 {
 	int	flag_q;
 	int	i;
 
-
 	if (!line)
 		return (NULL);
 	flag_q = 0;
-	i = j;
+	i = *j;
 	while (line[*j] && (line[*j] != ' ' || flag_q))
 	{
-		
+		if (line[*j] == '\'' && !flag_q)
+			flag_q = 1;
+		if (line[*j] == '"' && !flag_q)
+			flag_q = 2;
+		if (line[*j] == '$' && flag_q != 1)
+			get_variable(env, line, j, &i);
+		i++;
+		j++;
+		if (line && ((line[*j] == '\'' && flag_q == 1) || (line && line[*j] == '"' && flag_q == 2)))
+		{
+			flag_q = 0;
+			j++;
+		}
 	}
+	return(sh_strdup(line + i, *j - i));
 }
 
-char **get_argv(char *line, t_minishell *vars)
+char **get_argv(char *line, t_minishell *vars, char **env)
 {
 	int		i;
 	int		j;
@@ -114,24 +189,25 @@ char **get_argv(char *line, t_minishell *vars)
 	{
 		while (line[j] && line[j] == ' ')
 			j++;
-		vars->argv[i] = get_word_sh(line, &j);
+		vars->argv[i] = get_token_sh(line, &j, env);
 		i++;
 	}
 	vars->argv[i] = NULL;
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
 	t_minishell	vars;
 	char		*line;
 
 	line = NULL;
 	(void)argv;
+	vars.env = env;
 	printf("argc is %d\n", argc);
 	while (1)
 	{
 		line = readline(NULL);
-		vars.argv = get_argv(line, &vars);
+		vars.argv = get_argv(line, &vars, env);
 		free(line);
 	}
 	return 0;
--- test.c
+++ test.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aatieh <aatieh@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/04 00:32:11 by aatieh            #+#    #+#             */
-/*   Updated: 2025/01/13 01:46:51 by aatieh           ###   ########.fr       */
+/*   Created: 2025/01/13 01:25:48 by aatieh            #+#    #+#             */
+/*   Updated: 2025/01/13 01:39:22 by aatieh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "libft/inc/libft.h"
+#include <stdio.h>
 
-# include "../libft/inc/libft.h"
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
-typedef struct s_minishell
+int	main(int argc, char **argv)
 {
-	int		argc;
-	char	**argv;
-	char	**env;
-}			t_minishell;
+	int	i;
 
-#endif
+	i = 1;
+	while (i < argc)
+	{
+		printf("%s", argv[i]);
+		i++;
+		if (i < argc)
+			printf(" ");
+	}
+	printf("\n");
+	return 0;
+}
