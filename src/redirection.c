2db14ddae4d4d7f77db597d55e8f920c7e6035dd
Amjad <amjdsalh@gmail.com>
2025-02-10 17:14:05 +0300

    redirection now detect the redirections with no spaces in between

--- src/redirection.c
+++ src/redirection.c
@@ -6,7 +6,7 @@
 /*   By: aatieh <aatieh@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 19:29:46 by aatieh            #+#    #+#             */
-/*   Updated: 2025/02/09 06:49:59 by aatieh           ###   ########.fr       */
+/*   Updated: 2025/02/10 17:03:28 by aatieh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,9 @@ t_redirect	*get_redirection(char *line, int i, int op_num)
 		redirect->redirection[j] = line[i + j];
 		j++;
 	}
-	while (line[i + space_num + j] && line[i + space_num + j] != ' ')
+	while (line[i + space_num + j] && line[i + space_num + j] != ' '
+		&& line[i + space_num + j] != '|' && line[i + space_num + j] != '<'
+		&& line[i + space_num + j] != '>')
 	{
 		redirect->redirection[j] = line[i + space_num + j];
 		j++;
@@ -75,6 +77,7 @@ t_redirect	*get_redirections(char *line)
 	int			i;
 	int			op_num;
 	t_redirect	*head;
+	t_redirect	*tmp;
 
 	if (!line)
 		return (NULL);
@@ -84,8 +87,12 @@ t_redirect	*get_redirections(char *line)
 	while (line[i])
 	{
 		if (line[i] == '>' || line[i] == '<')
-			redirectionadd_back(&head, get_redirection(line, i, op_num));
-		if (line[i] == '|')
+		{
+			tmp = get_redirection(line, i, op_num);
+			redirectionadd_back(&head, tmp);
+			i--;
+		}
+		else if (line[i] == '|')
 			op_num++;
 		i++;
 	}
